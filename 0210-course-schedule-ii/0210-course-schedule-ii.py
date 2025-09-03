8129507295c2d7a0170181b18f1dc25ff992853e
Suman Hazra <sumanhazra852@gmail.com>
2025-09-03 12:42:39 +0530

    Time: 2 ms (81.04%), Space: 18.9 MB (95.49%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,6 +1,7 @@
+from collections import deque
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph={i:[] for i in range(numCourses)}
+        '''graph={i:[] for i in range(numCourses)}
         for course, prereq in prerequisites:
             graph[prereq].append(course)
 
@@ -23,5 +24,26 @@ class Solution:
             if visited[i]==0:
                 if dfs(i):
                     return []
-        return order[::-1]
-        
+        return order[::-1]'''
+        graph={i:[] for i in range(numCourses)}
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
+        indegree=[0]*numCourses
+        for first,second in prerequisites:
+            indegree[first]+=1
+        queue=deque()
+        for i in range(numCourses):
+            if indegree[i]==0:
+                queue.append(i)
+        ans=[]
+        while queue:
+            item=queue.popleft()
+            ans.append(item)
+            for i in graph[item]:
+                indegree[i]-=1
+                if indegree[i]==0:
+                    queue.append(i)
+        if len(ans)!=numCourses:
+            return []
+        return ans
+            
