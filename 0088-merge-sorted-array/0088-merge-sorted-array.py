7961c0394335f104b4fee0e801ab86e288489bcf
Suman Hazra <sumanhazra852@gmail.com>
2025-09-13 21:37:15 +0530

    Time: 0 ms (100%), Space: 17.9 MB (25.74%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,7 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        left=m-1
+        '''left=m-1
         right=0
         while left>=0 and right<n:
             if nums1[left]>nums2[right]:
@@ -15,5 +15,28 @@ class Solution:
 
         nums1[:m]=sorted(nums1[:m])
         nums2.sort()
-        nums1[m:]=nums2
-        
+        nums1[m:]=nums2'''
+        def swap(nums1,nums2,left,right):
+            if nums1[left]>nums2[right]:
+                nums1[left],nums2[right]=nums2[right],nums1[left]
+        length=m+n
+        gap=(length//2)+(length%2)
+        while gap>0:
+            left=0
+            right=left+gap
+            while right<length:
+                if left<m and right>=m:
+                    swap(nums1,nums2,left,right-m)
+                    left+=1
+                    right+=1
+                elif left>=m:
+                    swap(nums2,nums2,left-m,right-m)
+                    left+=1
+                    right+=1
+                else:
+                    swap(nums1,nums1,left,right)
+                    left+=1
+                    right+=1
+            if gap==1: break
+            gap=(gap//2)+(gap%2)
+        nums1[m:]=nums2                  
