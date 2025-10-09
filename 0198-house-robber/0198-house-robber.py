bc66e455c6ced06d2b01160e0defee9109a67eac
Suman Hazra <sumanhazra852@gmail.com>
2025-10-09 20:04:11 +0530

    Time: 0 ms (100%), Space: 17.7 MB (53.96%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,12 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n=len(nums)
-        dp=[-1]*(n+1)
-        dp[0]=nums[0]
-        for i in range(1,n):
+        prev=nums[0]
+        prev2=0
+        for i in range(1,len(nums)):
             take=nums[i]
-            if i>1: take+=dp[i-2]
-            non_take=dp[i-1]
-            dp[i]=max(take,non_take)
-        return dp[n-1]
+            if i>1: take+=prev2
+            non_take=prev
+            curr=max(take,non_take)
+
+            prev2=prev
+            prev=curr
+        return prev
+        
         
