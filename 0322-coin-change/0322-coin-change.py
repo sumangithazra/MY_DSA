84344acb81e6e9be2d1ee8d2a40b8a70f6c625e8
Suman Hazra <sumanhazra852@gmail.com>
2025-10-25 12:13:51 +0530

    Time: 860 ms (35.29%), Space: 18.5 MB (37.06%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -10,10 +10,11 @@ class Solution:
                 amount-=coins[i]
         if amount!=0: return -1
         return count'''
-        n=len(coins)
+        '''n=len(coins)
         dp=[[1e9]*(amount+1) for _ in range(n)]
         for i in range(amount+1):
             if i%coins[0]==0: dp[0][i]=i//coins[0]
+            else: dp[0][T]=1e9
         for idx in range(1,n):
             for target in range(amount+1):
                 not_take=0+dp[idx-1][target]
@@ -22,6 +23,24 @@ class Solution:
                     take=1+dp[idx][target-coins[idx]]
                 dp[idx][target]=min(take,not_take)
         ans=dp[n-1][amount]
+        return -1 if ans>=1e9 else ans'''
+        n=len(coins)
+        prev=[1e9]*(amount+1)
+        curr=[1e9]*(amount+1)
+        #dp=[[1e9]*(amount+1) for _ in range(n)]
+        for i in range(amount+1):
+            if i%coins[0]==0: prev[i]=i//coins[0]
+            else: prev[i]=1e9
+
+        for idx in range(1,n):
+            for target in range(amount+1):
+                not_take=0+prev[target]
+                take=1e9
+                if coins[idx]<=target:
+                    take=1+curr[target-coins[idx]]
+                curr[target]=min(take,not_take)
+            prev=curr[:]
+        ans=prev[amount]
         return -1 if ans>=1e9 else ans
 
 
