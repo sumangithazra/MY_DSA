a60dc995902c9e456e421d0b15d2b15c4130965f
Suman Hazra <sumanhazra852@gmail.com>
2025-11-03 19:48:02 +0530

    Time: 396 ms (91.3%), Space: 17.8 MB (94.35%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -9,7 +9,7 @@ class Solution:
         n=len(self.s)
         m=len(self.t)
         #return self.func(n,m)
-        dp=[[-1]*(m+1) for i in range(n+1)]
+        '''dp=[[-1]*(m+1) for i in range(n+1)]
         for i in range(n+1): dp[i][0]=0
         for j in range(m+1): dp[0][j]=0
         for i in range(1,n+1):
@@ -18,4 +18,15 @@ class Solution:
                     dp[i][j]=1+dp[i-1][j-1]
                 else:
                     dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-        return dp[n][m]
+        return dp[n][m]'''
+        prev=[0]*(m+1)
+        curr=[0]*(m+1)
+        for j in range(m+1): prev[j]=0
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if self.s[i-1]==self.t[j-1]:
+                    curr[j]=1+prev[j-1]
+                else:
+                    curr[j]=max(prev[j],curr[j-1])
+            prev=curr[:]
+        return prev[m]
