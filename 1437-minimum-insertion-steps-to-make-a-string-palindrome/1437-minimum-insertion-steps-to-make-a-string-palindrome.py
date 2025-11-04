05e573233bc9f006c824b3bc621b3ca07bdb370d
Suman Hazra <sumanhazra852@gmail.com>
2025-11-04 10:39:55 +0530

    Time: 563 ms (63.71%), Space: 17.6 MB (99.6%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -2,7 +2,7 @@ class Solution:
     def minInsertions(self, s: str) -> int:
         n=len(s)
         t=s[::-1]
-        dp=[[0]*(n+1) for _ in range(n+1)]
+        '''dp=[[0]*(n+1) for _ in range(n+1)]
         for i in range(n+1): dp[i][0]=0
         for j in range(n+1): dp[0][j]=0
         for i in range(1,n+1):
@@ -11,5 +11,17 @@ class Solution:
                     dp[i][j]=1+dp[i-1][j-1]
                 else:
                     dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-        return len(s)-dp[n][n]
+        return len(s)-dp[n][n]'''
+
+        prev=[0]*(n+1)
+        curr=[0]*(n+1)
+        for j in range(n+1): prev[j]=0
+        for i in range(1,n+1):
+            for j in range(1,n+1):
+                if s[i-1]==t[j-1]:
+                    curr[j]=1+prev[j-1]
+                else:
+                    curr[j]=max(prev[j],curr[j-1])
+            prev=curr[:]
+        return len(s)-prev[n]
         
