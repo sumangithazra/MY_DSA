2fb936a24facf041752c5f168c21e5c6de0fa1ab
Suman Hazra <sumanhazra852@gmail.com>
2025-11-13 01:03:58 +0530

    Time: 18 ms (78.11%), Space: 19.1 MB (29.16%) - LeetHub

---
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,51 @@
+class DisjointSet:
+    def __init__(self,n):
+        self.parent=[i for i in range(n)]
+        self.rank=[0]*n
+        self.size=[1]*n
+    def findUparent(self,node):
+        if self.parent[node]!=node:
+            self.parent[node]=self.findUparent(self.parent[node])
+        return self.parent[node]
+    def UnionByRank(self,u,v):
+        pu=self.findUparent(u)
+        pv=self.findUparent(v)
+        if pu==pv:
+            return
+        if self.rank[pu]>self.rank[pv]:
+            self.parent[pv]=pu
+        elif self.rank[pu]<self.rank[pv]:
+            self.parent[pu]=pv
+        else:
+            self.parent[pv]=pu
+            self.rank[pu]+=1
+    def UnionBySize(self,u,v):
+        pu=self.findUparent(u)
+        pv=self.findUparent(v)
+        if pu==pv:
+            return
+        if self.size[pu]<self.size[pv]:
+            self.parent[pu]=pv
+            self.size[pv]+=self.size[pu]
+        else:
+            self.parent[pv]=pu
+            self.size[pu]+=self.size[pv]
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        maxRow=-1
+        maxCol=-1
+        nodes=set()
+        for row, col in stones:
+            maxRow=max(maxRow,row)
+            maxCol=max(maxCol,col)
+        dsu=DisjointSet(maxRow+maxCol+2)
+        for row, col in stones:
+            rowNode=row
+            colNode=col+maxRow+1
+            dsu.UnionByRank(rowNode,colNode)
+            nodes.add(rowNode)
+            nodes.add(colNode)
+        components=len(set(dsu.findUparent(node) for node in nodes))
+        return len(stones)-components
+
+        
