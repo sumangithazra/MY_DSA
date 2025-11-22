2da79b19edb2615ba7f6196afef5cf1652219a06
Suman Hazra <sumanhazra852@gmail.com>
2025-11-22 18:31:59 +0530

    Time: 0 ms (100%), Space: 17.7 MB (86.33%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -13,7 +13,19 @@ class Solution:
         if len(nums)==1: return nums[0]
         temp1=nums[:n-1]
         temp2=nums[1:n]
-        dp1=[-1]*(len(temp1))
-        dp2=[-1]*(len(temp2))
-        return max(self.func(len(temp1)-1,temp1,dp1),self.func(len(temp2)-1,temp2,dp2))
+        #dp1=[-1]*(len(temp1))
+        #dp2=[-1]*(len(temp2))
+        #return max(self.func(len(temp1)-1,temp1,dp1),self.func(len(temp2)-1,temp2,dp2))
+        return max(self.Helper(temp1),self.Helper(temp2))
+    def Helper(self,arr):
+        n=len(arr)
+        dp=[0]*n
+        dp[0]=arr[0]
+        for i in range (1,n):
+            not_take=dp[i-1]
+            take=arr[i]
+            if i-2>=0:
+                take+=dp[i-2]
+            dp[i]=max(take,not_take)
+        return dp[n-1]
         
