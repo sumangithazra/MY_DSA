2fa0de65f33229fd0696e1982b8274767f12cb04
Suman Hazra <sumanhazra852@gmail.com>
2025-11-22 18:24:54 +0530

    Time: 4 ms (4.7%), Space: 17.6 MB (97.91%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,20 +1,19 @@
 class Solution:
+    def func(self,i,nums,dp):
+        if i==0: return nums[i]
+        if i<0: return 0
+        if dp[i]!=-1: return dp[i]
+        not_take=self.func(i-1,nums,dp)
+        take=nums[i]+self.func(i-2,nums,dp)
+        dp[i]=max(take,not_take)
+        return dp[i]
     def rob(self, nums: List[int]) -> int:
+        n=len(nums)
+        if len(nums)==0: return 0
         if len(nums)==1: return nums[0]
-        temp1=nums[:len(nums)-1]
-        temp2=nums[1:]
-        return max(self.Finding_max(temp1),self.Finding_max(temp2))
-    
-    def Finding_max(self,var):
-        prev=var[0]
-        prev2=0
-        for i in range(1,len(var)):
-            take=var[i]
-            if i>1: take+=prev2
-            non_take=prev
-            curr=max(take,non_take)
-            prev2=prev
-            prev=curr
-        return prev
-
+        temp1=nums[:n-1]
+        temp2=nums[1:n]
+        dp1=[-1]*(len(temp1))
+        dp2=[-1]*(len(temp2))
+        return max(self.func(len(temp1)-1,temp1,dp1),self.func(len(temp2)-1,temp2,dp2))
         
