baab7b778283f241b009280f7ea023bfcbdd2aeb
Suman Hazra <sumanhazra852@gmail.com>
2025-11-22 19:05:17 +0530

    Time: 0 ms (100%), Space: 18 MB (30.14%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,6 +1,38 @@
 class Solution:
+    def func(self,i,j):
+        if self.obstacleGrid[i][j]==1: return 0
+        if i==0 and j==0: return 1
+        if i<0 or j<0 : return 0
+        if self.dp[i][j]!=-1: return self.dp[i][j]
+        up=self.func(i-1,j)
+        left=self.func(i,j-1)
+        self.dp[i][j]=up+left
+        return self.dp[i][j]
+
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        #self.obstacleGrid=obstacleGrid
+        #m=len(self.obstacleGrid)
+        #n=len(self.obstacleGrid[0])
+        #self.dp=[[-1]*n for _ in range(m)]
+        #return self.func(m-1,n-1)
         m=len(obstacleGrid)
+        n=len(obstacleGrid[0])
+        dp=[[0]*n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j]==1:
+                    dp[i][j]=0
+                elif i==0 and j==0:
+                    dp[i][j]=1
+                else:
+                    up=0
+                    left=0
+                    if i>0: up=dp[i-1][j]
+                    if j>0: left=dp[i][j-1]
+                    dp[i][j]=up+left
+        return dp[m-1][n-1]
+
+        '''m=len(obstacleGrid)
         n=len(obstacleGrid[0])
         #dp=[[-1]*n for _ in range(m)]
         prev=[0]*n
@@ -16,6 +48,6 @@ class Solution:
                     if j>0: left=curr[j-1]
                     curr[j]=up+left
             prev=curr
-        return prev[n-1]
+        return prev[n-1]'''
 
         
