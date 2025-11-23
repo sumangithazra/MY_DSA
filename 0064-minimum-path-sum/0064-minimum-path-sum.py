6d53e4fd129ddb5fde27c778cd65a091d066ef62
Suman Hazra <sumanhazra852@gmail.com>
2025-11-23 14:30:02 +0530

    Time: 31 ms (24.43%), Space: 20.4 MB (30.29%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,6 +1,19 @@
 class Solution:
+    def func(self,i,j):
+        if i==0 and j==0: return self.grid[i][j]
+        if i<0 or j<0: return 1e9
+        if self.dp[i][j]!=-1: return self.dp[i][j]
+        up=self.grid[i][j]+ self.func(i-1,j)
+        left=self.grid[i][j] + self.func(i,j-1)
+        self.dp[i][j]=min(up,left)
+        return self.dp[i][j]
     def minPathSum(self, grid: List[List[int]]) -> int:
+        self.grid=grid
         m=len(grid)
+        n=len(grid[0])
+        self.dp=[[-1]*n for _ in range(m)]
+        return self.func(m-1,n-1)
+        '''m=len(grid)
         n=len(grid[0])
         dp=[[-1]*n for _ in range(m)]
         for i in range(m):
@@ -16,5 +29,5 @@ class Solution:
                 if j>0: left+=dp[i][j-1]
                 else: left=1e9
                 dp[i][j]=min(up,left)
-        return dp[m-1][n-1] 
+        return dp[m-1][n-1] '''
         
