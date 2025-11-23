18be49fc048d07f87154c4ad9b589857e2afb080
Suman Hazra <sumanhazra852@gmail.com>
2025-11-23 18:23:55 +0530

    Time: 1804 ms (21.99%), Space: 33.3 MB (37.68%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,22 +1,39 @@
 class Solution:
-    def canPartition(self, nums: List[int]) -> bool:
-        total_sum=0
-        for i in range(len(nums)):
-            total_sum+=nums[i]
-        if total_sum%2==1: return False
-        return self.sub_sum_k(nums,total_sum//2)
-    def sub_sum_k(self,nums,k):
-        n=len(nums)
-        dp=[[False]*(k+1) for _ in range(n)]
-        for i in range(n):
-            dp[i][0]=True
-        if nums[0]<=k: dp[0][nums[0]]=True
-        for idx in range(1,n):
-            for target in range(1,k+1):
+    def func(self,idx,target):
+        if target==0: return True
+        if idx==0: return self.nums[idx]==target
+        if self.dp[idx][target]!=False: return self.dp[idx][target]
+        not_take=self.func(idx-1,target)
+        take=False
+        if self.nums[idx]<=target:
+            take=self.func(idx-1,target-self.nums[idx])
+        self.dp[idx][target]=take or not_take
+        return self.dp[idx][target]
+    def CheckSum(self,sumi):
+        dp=[[False]*(sumi+1) for _ in range(self.n)]
+        for idx in range(self.n):
+            dp[idx][0]=True
+        if self.nums[0]<=sumi:
+            dp[0][self.nums[0]]=True
+        for idx in range(self.n):
+            for target in range(1,sumi+1):
                 not_take=dp[idx-1][target]
                 take=False
-                if nums[idx]<=target:
-                    take=dp[idx-1][target-nums[idx]]
-                dp[idx][target]=not_take or take
-        return dp[n-1][k]
+                if self.nums[idx]<=target:
+                    take=dp[idx-1][target-self.nums[idx]]
+                dp[idx][target]=take or not_take
+        return dp[self.n-1][sumi]
+
+
+    def canPartition(self, nums: List[int]) -> bool:
+        self.nums=nums
+        #n=len(nums)
+        self.n=len(nums)
+        sumi=0
         
+        for i in self.nums:
+            sumi+=i
+        if sumi%2==1: return False
+        #self.dp=[[False]*(sumi//2 +1) for _ in range(n)]
+        #return self.func(n-1,sumi//2)
+        return self.CheckSum(sumi//2)
