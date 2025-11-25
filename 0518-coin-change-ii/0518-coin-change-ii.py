868bf3761160e783652f7ebd2168f967062f57cd
Suman Hazra <sumanhazra852@gmail.com>
2025-11-25 22:09:37 +0530

    Time: 455 ms (45.87%), Space: 65.4 MB (25.5%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -12,22 +12,25 @@ class Solution:
         self.dp[idx][target]=take+not_take
         return self.dp[idx][target]
     def change(self, amount: int, coins: List[int]) -> int:
+        #n=len(coins)
+        #self.coins=coins
+        #self.dp=[[-1]*(amount+1) for _ in range(n)]
+        #return self.func(n-1,amount)
         n=len(coins)
-        self.coins=coins
-        self.dp=[[-1]*(amount+1) for _ in range(n)]
-        return self.func(n-1,amount)
-        '''n=len(coins)
         dp=[[0]*(amount+1) for _ in range(n)]
+        for idx in range(n):
+            dp[idx][0]=1
         for target in range(amount+1):
-            dp[0][target]= 1 if target%coins[0]==0 else 0
-        for idx in range(1,n):
-            for target in range(amount+1):
-                not_take=dp[idx-1][target]
+            if target % coins[0]==0:
+                dp[0][target]=1
+        for i in range(1,n):
+            for target in range(1,amount+1):
+                not_take=dp[i-1][target]
                 take=0
-                if coins[idx]<=target:
-                    take=dp[idx][target-coins[idx]]
-                dp[idx][target]=take+not_take
-        return dp[n-1][amount]'''
+                if coins[i]<=target:
+                    take=dp[i][target-coins[i]]
+                dp[i][target]=take+not_take
+        return dp[n-1][amount]
         '''n=len(coins)
         #dp=[[0]*(amount+1) for _ in range(n)]
         prev=[0]*(amount+1)
