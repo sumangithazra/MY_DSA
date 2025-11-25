f3d367fcb5e172cd745ab02ac212251aac8ad7ce
Suman Hazra <sumanhazra852@gmail.com>
2025-11-25 21:54:31 +0530

    Time: 885 ms (33.14%), Space: 19.7 MB (28.31%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -13,11 +13,28 @@ class Solution:
         self.dp[idx][target]=min(take,not_take)
         return self.dp[idx][target]
     def coinChange(self, coins: List[int], amount: int) -> int:
+        #n=len(coins)
+        #self.coins=coins
+        #self.dp=[[-1]*(amount+1) for _ in range(n)]
+        #ans=self.func(n-1,amount)
+        #return -1 if ans==1e9 else ans
         n=len(coins)
-        self.coins=coins
-        self.dp=[[-1]*(amount+1) for _ in range(n)]
-        ans=self.func(n-1,amount)
+        dp=[[1e9]*(amount+1) for _ in range(n)]
+        for idx in range(n):
+            dp[idx][0]=0
+        for target in range(1,amount+1):
+            if target % coins[0] ==0:
+                dp[0][target]=target//coins[0]
+        for i in range(1,n):
+            for target in range(1,amount+1):
+                not_take=dp[i-1][target]
+                take=1e9
+                if coins[i]<=target:
+                    take=1+dp[i][target-coins[i]]
+                dp[i][target]=min(take,not_take)
+        ans=dp[n-1][amount]
         return -1 if ans==1e9 else ans
+
 
 
         
