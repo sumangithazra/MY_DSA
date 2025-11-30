ff0a3ed7810d88e2ac2c623097d464942bf2bc5f
Suman Hazra <sumanhazra852@gmail.com>
2025-11-30 10:27:55 +0530

    Time: 125 ms (56.6%), Space: 20 MB (46.01%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -1,26 +1,18 @@
 class Solution:
+    def func(self,i,j):
+        if i==0 or j==0: return 0
+        if self.dp[i][j]!=-1: return self.dp[i][j]
+        if self.word1[i-1]==self.word2[j-1]:
+            self.dp[i][j]= 1+self.func(i-1,j-1)
+            return self.dp[i][j]
+        else:
+            self.dp[i][j]= max(self.func(i-1,j),self.func(i,j-1))
+            return self.dp[i][j]
     def minDistance(self, word1: str, word2: str) -> int:
+        self.word1=word1
+        self.word2=word2
         n=len(word1)
         m=len(word2)
-        '''dp=[[0]*(m+1) for _ in range(n+1)]
-        for i in range(n+1): dp[i][0]=0
-        for j in range(m+1): dp[0][j]=0
-        for i in range(1,n+1):
-            for j in range(1,m+1):
-                if word1[i-1]==word2[j-1]:
-                    dp[i][j]=1+dp[i-1][j-1]
-                else:
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-        return (m+n)-(2*dp[n][m])'''
-        prev=[0]*(m+1)
-        curr=[0]*(m+1)
-        for j in range(m+1): prev[j]=0
-        for i in range(1,n+1):
-            for j in range(1,m+1):
-                if word1[i-1]==word2[j-1]:
-                    curr[j]=1+prev[j-1]
-                else:
-                    curr[j]=max(prev[j],curr[j-1])
-            prev=curr[:]
-        return (m+n)-(2*prev[m])
+        self.dp=[[-1]*(m+1) for _ in range(n+1)]
+        return (m+n)- 2*self.func(n,m)
         
