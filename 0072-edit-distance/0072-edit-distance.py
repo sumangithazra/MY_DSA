b852e945d5bd008b053dbf655461d6871fa11f43
Suman Hazra <sumanhazra852@gmail.com>
2025-11-30 11:45:17 +0530

    Time: 30 ms (94.96%), Space: 19.6 MB (77.94%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,28 +1,22 @@
 class Solution:
+    def func(self,i,j):
+        if i==0: return j
+        if j==0: return i
+        if self.dp[i][j]!=-1: return self.dp[i][j]
+
+        if self.word1[i-1]==self.word2[j-1]:
+            self.dp[i][j]= self.func(i-1,j-1)
+            return self.dp[i][j]
+        else:
+            insert=1+self.func(i,j-1)
+            delete=1+self.func(i-1,j)
+            replace=1+ self.func(i-1,j-1)
+            self.dp[i][j]= min(insert,delete, replace)
+            return self.dp[i][j]
     def minDistance(self, word1: str, word2: str) -> int:
+        self.word1=word1
+        self.word2=word2
         n=len(word1)
         m=len(word2)
-        '''dp=[[0]*(m+1) for _ in range(n+1)]
-        for i in range(n+1): dp[i][0]=i
-        for j in range(m+1): dp[0][j]=j
-        for i in range(1,n+1):
-            for j in range(1,m+1):
-                if word1[i-1]==word2[j-1]:
-                    dp[i][j]=dp[i-1][j-1]
-                else:
-                    dp[i][j]=1+min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])
-        return dp[n][m]'''
-        prev=[0]*(m+1)
-        curr=[0]*(m+1)
-        for j in range(m+1): prev[j]=j
-        for i in range(1,n+1):
-            curr[0]=i
-            for j in range(1,m+1):
-                if word1[i-1]==word2[j-1]:
-                    curr[j]=prev[j-1]
-                else:
-                    curr[j]=1+min(prev[j],curr[j-1],prev[j-1])
-            prev=curr[:]
-        return prev[m]
-        
-        
+        self.dp=[[-1]*(m+1) for _ in range(n+1)]
+        return self.func(n,m)
