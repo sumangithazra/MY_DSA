89202d3abd3320cb91ad8aff732446a3f1802094
Suman Hazra <sumanhazra852@gmail.com>
2025-11-30 12:07:35 +0530

    Time: 59 ms (68.8%), Space: 17.8 MB (96.1%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -20,7 +20,7 @@ class Solution:
         #m=len(word2)
         #self.dp=[[-1]*(m+1) for _ in range(n+1)]
         #return self.func(n,m)
-        n=len(word1)
+        '''n=len(word1)
         m=len(word2)
         dp=[[0]*(m+1) for _ in range(n+1)]
         for i in range(n+1):
@@ -36,4 +36,22 @@ class Solution:
                     delete=1+dp[i-1][j]
                     replace=1+dp[i-1][j-1]
                     dp[i][j]=min(insert, delete, replace)
-        return dp[n][m]
+        return dp[n][m]'''
+        n=len(word1)
+        m=len(word2)
+        prev=[0]*(m+1)
+        curr=[0]*(m+1)
+        for j in range(m+1):
+            prev[j]=j
+        for i in range(1,n+1):
+            curr[0]=i
+            for j in range(1,m+1):
+                if word1[i-1]==word2[j-1]:
+                    curr[j]=prev[j-1]
+                else:
+                    insert=1+curr[j-1]
+                    delete=1+prev[j]
+                    replace=1+prev[j-1]
+                    curr[j]=min(insert, delete, replace)
+            prev=curr[:]
+        return prev[m]
