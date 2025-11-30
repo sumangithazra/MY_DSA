3b2ddd01449f78474decdcf9d1d8ba52af4ad628
Suman Hazra <sumanhazra852@gmail.com>
2025-11-30 10:20:16 +0530

    Time: 563 ms (63.14%), Space: 18.1 MB (83.94%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -14,7 +14,7 @@ class Solution:
         #self.t=s[::-1]
         #self.dp=[[-1]*(n+1) for _ in range(n+1)]
         #return n-self.func(n,n)
-        n=len(s)
+        '''n=len(s)
         t=s[::-1]
         dp=[[0]*(n+1) for _ in range(n+1)]
         for i in range(1,n+1):
@@ -23,6 +23,18 @@ class Solution:
                     dp[i][j]=1+ dp[i-1][j-1]
                 else:
                     dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-        return n-dp[n][n]
+        return n-dp[n][n]'''
+        n=len(s)
+        t=s[::-1]
+        prev=[0]*(n+1)
+        curr=[0]*(n+1)
+        for i in range(1,n+1):
+            for j in range(1,n+1):
+                if s[i-1]==t[j-1]:
+                    curr[j]=1+ prev[j-1]
+                else:
+                    curr[j]=max(prev[j],curr[j-1])
+            prev=curr[:]
+        return n-prev[n]
 
         
