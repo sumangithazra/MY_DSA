099807857b046a5f5d6dfbb786152ba09d04b125
Suman Hazra <sumanhazra852@gmail.com>
2025-11-30 12:24:02 +0530

    Time: 275 ms (78.65%), Space: 49.1 MB (34.25%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -1,23 +1,27 @@
 class Solution:
+    def func(self,i,j):
+        if i==0 and j==0: return True
+        if i==0 and j>0:
+            while j>0:
+                if self.p[j-1]!='*':
+                    return False
+                j-=1
+            return True
+        if j==0: return False
+        if self.dp[i][j]!=-1: return self.dp[i][j]
+
+        if self.p[j-1]=='?' or self.s[i-1]==self.p[j-1]:
+            self.dp[i][j]= self.func(i-1,j-1)
+            return self.dp[i][j]
+        elif self.p[j-1]=='*':
+            self.dp[i][j]= self.func(i,j-1) or self.func(i-1,j)
+            return self.dp[i][j]
+        else: return False
     def isMatch(self, s: str, p: str) -> bool:
-        n=len(p)
-        m=len(s)
-        dp=[[False]*(m+1) for _ in range(n+1)]
-        dp[0][0]=True
-        for i in range(n+1):
-            flag=True
-            for ii in range(1,i+1):
-                if p[ii-1]!='*':
-                    flag=False
-                    break
-            dp[i][0]=flag
-        for i in range(1,n+1):
-            for j in range(1,m+1):
-                if p[i-1]==s[j-1] or p[i-1]=='?':
-                    dp[i][j]=dp[i-1][j-1]
-                elif p[i-1]=="*":
-                    dp[i][j]=dp[i-1][j] or dp[i][j-1]
-                else:
-                    dp[i][j]=False
-        return dp[n][m]
+        self.s=s
+        self.p=p
+        n=len(s)
+        m=len(p)
+        self.dp=[[-1]*(m+1) for _ in range(n+1)]
+        return self.func(n,m)
         
