b9dabb52c6f89a74f4db6d5dec6c5e0cc6fa027f
Suman Hazra <sumanhazra852@gmail.com>
2025-11-30 10:33:02 +0530

    Time: 108 ms (90.35%), Space: 17.6 MB (99.2%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -15,7 +15,7 @@ class Solution:
         #m=len(word2)
         #self.dp=[[-1]*(m+1) for _ in range(n+1)]
         #return (m+n)- 2*self.func(n,m)
-        n=len(word1)
+        '''n=len(word1)
         m=len(word2)
         dp=[[0]*(m+1) for _ in range(n+1)]
         for i in range(1,n+1):
@@ -24,5 +24,19 @@ class Solution:
                     dp[i][j]=1+dp[i-1][j-1]
                 else:
                     dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-        return (n+m)- 2*dp[n][m]
+        return (n+m)- 2*dp[n][m]'''
+
+        n=len(word1)
+        m=len(word2)
+        prev=[0]*(m+1)
+        curr=[0]*(m+1)
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if word1[i-1]==word2[j-1]:
+                    curr[j]=1+prev[j-1]
+                else:
+                    curr[j]=max(prev[j],curr[j-1])
+            prev=curr[:]
+        return (n+m)- 2*prev[m]
+        
         
