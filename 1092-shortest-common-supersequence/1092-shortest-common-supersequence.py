ccbbdd532cf2ffe22758a2a68d4c53c152fbe4d8
Suman Hazra <sumanhazra852@gmail.com>
2025-11-30 11:05:55 +0530

    Time: 395 ms (82.5%), Space: 42.8 MB (65.97%) - LeetHub

---
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
@@ -0,0 +1,32 @@
+class Solution:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        n=len(str1)
+        m=len(str2)
+        dp=[[0]*(m+1) for _ in range(n+1)]
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if str1[i-1]==str2[j-1]:
+                    dp[i][j]=1+dp[i-1][j-1]
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        final=[]
+        i=n
+        j=m
+        while i>0 and j>0:
+            if str1[i-1]==str2[j-1]:
+                final.append(str1[i-1])
+                i-=1
+                j-=1
+            elif dp[i-1][j]> dp[i][j-1]:
+                final.append(str1[i-1])
+                i-=1
+            else:
+                final.append(str2[j-1])
+                j-=1
+        while i>0:
+            final.append(str1[i-1])
+            i-=1
+        while j>0:
+            final.append(str2[j-1])
+            j-=1
+        return final[::-1]       
